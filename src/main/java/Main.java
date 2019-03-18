739f3aac6c543885c8e87cf82e00c9d4b8e54b50
Alk2017 <alk93@rambler.ru>
2019-03-18 19:27:13 +0300

    a

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.eclipse.jetty.server.Server;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Frontend frontend = new Frontend();
+
+
+        Server server = new Server(8080);
+
+
+
+    }
+}
--- src/main/java/helloMain.java
+++
@@ -1,6 +0,0 @@
-public class helloMain {
-
-    public static void main(String[] args) {
-        System.out.println("Hello alkalk");
-    }
-}
