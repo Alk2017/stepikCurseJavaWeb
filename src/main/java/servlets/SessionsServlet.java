7cdab5e705a52aa86b19b644df37cfa05d494d0f
alk-win <alk93@rambler.ru>
2019-04-20 11:13:34 +0300

    - добавлены сервлеты для задания по авторизации

---
+++ src/main/java/servlets/SessionsServlet.java
@@ -0,0 +1,83 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.google.gson.Gson;
+
+public class SessionsServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SessionsServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //get logged user profile
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String sessionId = request.getSession().getId();
+        UserProfile profile = accountService.getUserBySessionId(sessionId);
+        if (profile == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            Gson gson = new Gson();
+            String json = gson.toJson(profile);
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Hello!");
+            response.getWriter().println(json);
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
+
+    //sign in
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if (login == null || password == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile == null || !profile.getPassword().equals(password)) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        accountService.addSession(request.getSession().getId(), profile);
+        Gson gson = new Gson();
+        String json = gson.toJson(profile);
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("Glad to see you, again!");
+        response.getWriter().println(json);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    //sign out
+    public void doDelete(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        String sessionId = request.getSession().getId();
+        UserProfile profile = accountService.getUserBySessionId(sessionId);
+        if (profile == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            accountService.deleteSession(sessionId);
+            response.setContentType("text/html;charset=utf-8");
+            response.getWriter().println("Goodbye!");
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
+}
---
+++ src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignUpServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {this.accountService = accountService;}
+
+    //sign up
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if (login == null || password == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile profile = new UserProfile(login, password, "");
+        accountService.addNewUser(profile);
+/*
+        Gson gson = new Gson();
+        String json = gson.toJson(accountService.getUserByLogin(login));
+
+        response.getWriter().println("Hello, "+ login+"!");
+        response.getWriter().println("SignUp succes.");
+        response.getWriter().println(json);
+        */
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+
+    }
+}
